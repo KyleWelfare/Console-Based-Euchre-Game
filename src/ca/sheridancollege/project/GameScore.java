2cd94a4bc9b98394747b6f31af59946d3336539d
grandmasterming <theneoncolorz@gmail.com>
2021-04-14 19:38:13 -0400

    Initialized classes as singletons

--- src/ca/sheridancollege/project/GameScore.java
+++ src/ca/sheridancollege/project/GameScore.java
@@ -2,9 +2,21 @@ package ca.sheridancollege.project;
 
 public class GameScore extends Score {
 
-    public GameScore() {
-        // TODO - implement GameScore.GameScore
-        throw new UnsupportedOperationException();
+	//Singleton class
+	private static GameScore instance = null;
+	
+	//10 point win limit
+	private final int MAX_SCORE = 10;
+	
+    private GameScore() {
+		setScoreLimit(MAX_SCORE);
     }
+	
+	public static GameScore getInstance(){
+		if (instance == null)
+			instance = new GameScore();
+		
+		return instance;
+	}
 
 }
--- src/ca/sheridancollege/project/RoundScore.java
+++ src/ca/sheridancollege/project/RoundScore.java
@@ -2,9 +2,29 @@ package ca.sheridancollege.project;
 
 public class RoundScore extends Score {
 
-    public RoundScore() {
-        // TODO - implement RoundScore.RoundScore
-        throw new UnsupportedOperationException();
+	//Singleton class
+	private static RoundScore instance = null;
+	
+	//Max 5 tricks
+	private final int MAX_SCORE = 5;
+	
+    private RoundScore() {
+        setScoreLimit(MAX_SCORE);
     }
+	
+	public static RoundScore getInstance(){
+		if(instance == null){
+			instance = new RoundScore();
+		}
+		
+		return instance;
+	}
+	
+	//reset all round scores for new round
+	public void resetScores(){
+		setTeam1Score(0);
+		setTeam2Score(0);
+		setScoreLimit(5);
+	}
 
 }
