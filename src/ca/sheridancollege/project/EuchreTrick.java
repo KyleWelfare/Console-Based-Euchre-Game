8326d9850a3cf6a1274ecce3bff888894928d45c
Kyle Welfare <kyledwelfare@gmail.com>
2021-04-18 20:27:33 -0400

    EuchreTrick code added

--- src/ca/sheridancollege/project/EuchreTrick.java
+++ src/ca/sheridancollege/project/EuchreTrick.java
@@ -4,8 +4,7 @@ import ca.sheridancollege.project.Card.Suit;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class EuchreTrick {
-    private Suit trumpSuit;
+public class EuchreTrick {    
     private EuchreRound currRound;
     private Suit leadSuit = null;
     
@@ -16,15 +15,24 @@ public class EuchreTrick {
     public void promptUser(ArrayList<Player> players, int playerIndex) {
         System.out.println("Trump Suit: " + currRound.getTrumpSuit());
         if (this.leadSuit != null)
-            System.out.println("Lead Suit: " + this.leadSuit);
+            System.out.println("Leading Suit: " + this.leadSuit);
         
         if (currRound.getCardsPlayed().getCards() != null)
             System.out.println("Cards played this trick: " + currRound.getCardsPlayed().toString());
         
         System.out.println("Your Hand: ");
         ((EuchrePlayer) players.get(playerIndex)).getHand().toString();
-        
-        System.out.println("Enter the corresponding number to play a card");
 
+    }
+    
+    public void setWeightedValues(ArrayList<Player> players) {
+        for (int i = 0; i < players.size(); i++) {
+            ArrayList<Card> cards = ((EuchrePlayer)players.get(i)).getHand().getCards();
+            for (int j = 0; j < ((EuchrePlayer)players.get(i)).getHand().size; j++) {
+                if (cards.get(j).getSuit() == leadSuit && cards.get(j).getSuit() != currRound.getTrumpSuit() && !(cards.get(j).equals(currRound.getRightBower())) && !(cards.get(j).equals(currRound.getLeftBower()))) {
+                    ((EuchreCard)((EuchrePlayer)players.get(i)).getHand().getCards().get(j)).setWeightedValue(50);
+                }                
+            }       
+        }
     }
 }
